8b9161a9bc1644329453d7529a8e65ee35ffd417
jaime7981 <jmfernandez1@miuandes.cl>
2022-08-23 10:59:22 -0400

    feat: header template and other static files

--- ITS/settings.py
+++ ITS/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ITS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'DCL/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    "DCL/templates/static/",
+    "templates/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
