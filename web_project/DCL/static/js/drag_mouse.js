999c27d8d88d70b1cb21dd3a3673ceb2eebddc71
jaime7981 <jmfernandez1@miuandes.cl>
2022-09-03 20:54:59 -0400

    drawing shapes into canvas

--- web_project/DCL/static/js/drag_mouse.js
+++ web_project/DCL/static/js/drag_mouse.js
@@ -1,18 +1,57 @@
+// Init
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-
+var barButton = document.getElementById("select-bar-button");
+var circleButton = document.getElementById("select-circle-button");
 const WIDTH = 1900;
 const HEIGHT = 800;
 const STEP = 50;
-
+const LINE_WIDTH = 10;
 context.canvas.width  = WIDTH;
 context.canvas.height = HEIGHT;
-
-//style
 canvas.style.backgroundColor = 'white';
-//canvas.style.width = '500px';
-//canvas.style.height = '500px';
 
+var bar_list = [];
+var circle_list = [];
+var selected_shape = 'bar';
+
+// Classes 
+function Point(x,y) {
+    this.x = x;
+    this.y = y;
+}
+
+function Bar(init_coordinates, end_coordinates) {
+    this.init_x = init_coordinates[0];
+    this.init_y = init_coordinates[1];
+    this.end_x = end_coordinates[0];
+    this.end_y = end_coordinates[1];
+
+    this.draw = function() {
+        context.beginPath();
+        context.moveTo(this.init_x, this.init_y);
+        context.lineTo(this.end_x, this.end_y);
+        context.closePath();
+        context.strokeStyle = 'blue';
+        context.lineWidth = LINE_WIDTH;
+        context.stroke();
+    }
+}
+
+function Circle(init_coordinates) {
+    this.init_x = init_coordinates[0];
+    this.init_y = init_coordinates[1];
+
+    this.draw = function() {
+        context.beginPath();
+        context.arc(this.init_x, this.init_y, 10, 0, 2 * Math.PI); //(center_x, center_y, start_angle, end_angle)
+        context.closePath();
+        context.strokeStyle = 'red';
+        context.stroke();
+    }
+}
+
+// Code Functions
 var drawLine = function(canvas, context) {
     var startX = 0;
     var startY = 0;
@@ -21,12 +60,17 @@ var drawLine = function(canvas, context) {
     function drawDot(event) {
         if(dragging){
             context.clearRect(0,0,canvas.width, canvas.height)
-            drawGrid(context, STEP)
-            context.beginPath();
-            context.moveTo(startX, startY);
-            context.lineTo(event.offsetX, event.offsetY);
-            context.stroke();
-            context.closePath();
+            drawGrid(context, STEP);
+            drawAllObjects(bar_list);
+            drawAllObjects(circle_list);
+            if (selected_shape == 'bar'){
+                drawed_bar = new Bar([startX, startY], [event.offsetX, event.offsetY]);
+                drawed_bar.draw();
+            }
+            else if (selected_shape == 'circle'){
+                drawed_bar = new Circle([event.offsetX, event.offsetY]);
+                drawed_bar.draw();
+            }
         }
     }
 
@@ -34,47 +78,53 @@ var drawLine = function(canvas, context) {
         dragging = true;
         startX = event.offsetX;
         startY = event.offsetY;
+        if (selected_shape == "circle"){
+            drawed_bar = new Circle([event.offsetX, event.offsetY]);
+            drawed_bar.draw();
+        }
     }
 
-    function disengage(){
+    function disengage(event){
         dragging = false;
+        if (selected_shape == "bar"){
+            bar_list.push(new Bar([startX, startY], [event.offsetX, event.offsetY]));
+        }
+        else if (selected_shape == "circle"){
+            circle_list.push(new Circle([event.offsetX, event.offsetY]));
+        }
     }
 
     canvas.addEventListener("mousedown",engage);
     canvas.addEventListener("mouseup",disengage);
     canvas.addEventListener("mousemove",drawDot,false);
-    console.log('DrawLine');
 }
 
-// the render logic should be focusing on the rendering 
-var drawGrid = function(ctx, step) {
-    w = ctx.canvas.width
-    h = ctx.canvas.height
+var drawGrid = function(context, step) {
+    context.beginPath(); 
+    for (var x = 0; x <= WIDTH; x += step) {
+        context.moveTo(x, 0);
+        context.lineTo(x, HEIGHT);
+    }
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.stroke();
+    context.beginPath(); 
+    for (var y = 0; y <= HEIGHT; y += step) {
+        context.moveTo(0, y);
+        context.lineTo(WIDTH, y);
+    }
+    context.stroke();
+}
 
-    console.log('DrawGrid');
-    ctx.beginPath(); 
-    for (var x=0;x<=w;x+=step) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, h);
+var drawAllObjects = function(objects_list) {
+    for (object_item in objects_list) {
+        objects_list[object_item].draw();
     }
-    // set the color of the line
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
-    // the stroke will actually paint the current path 
-    ctx.stroke(); 
-    // for the sake of the example 2nd path
-    ctx.beginPath(); 
-    for (var y=0;y<=h;y+=step) {
-            ctx.moveTo(0, y);
-            ctx.lineTo(w, y);
-    }
-    // set the color of the line
-    ctx.strokeStyle = 'black';
-    // just for fun
-    ctx.lineWidth = 1;
-    // for your original question - you need to stroke only once
-    ctx.stroke();
-};
+}
+
+// Event Listeners
+barButton.addEventListener('click', function() {selected_shape = 'bar';}, false);
+circleButton.addEventListener('click', function() {selected_shape = 'circle';}, false);
 
 drawGrid(context, STEP);
 drawLine(canvas, context);
--- web_project/static_collect/js/draw_shapes.js
+++ web_project/static_collect/js/draw_shapes.js
@@ -1,4 +1,23 @@
-// Classes
+// Init
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+context.canvas.width  = WIDTH;
+context.canvas.height = HEIGHT;
+canvas.style.backgroundColor = 'aquamarine';
+
+const LINE_WIDTH = 20;
+var bar_list = [];
+var circle_list = [new Circle([100,600])];
+
+// Classes 
+function Point(x,y) {
+    this.x = x;
+    this.y = y;
+}
+
 function Bar(init_coordinates, end_coordinates) {
     this.init_x = init_coordinates[0];
     this.init_y = init_coordinates[1];
@@ -12,8 +31,29 @@ function Bar(init_coordinates, end_coordinates) {
         context.lineTo(this.end_x, this.end_y);
         context.closePath();
         context.strokeStyle = 'blue';
-        context.lineWidth = 4;
+        context.lineWidth = LINE_WIDTH;
         context.stroke();
+    }
+
+    this.check_area = function(mouse_coordinates) {
+        var mouse_x = mouse_coordinates[0];
+        var mouse_y = mouse_coordinates[1];
+        var check_x = mouse_x <= this.init_x && mouse_x >= this.end_x + LINE_WIDTH;
+        var check_y = mouse_y >= this.init_y && mouse_y >= this.end_y + LINE_WIDTH;
+        //console.log(check_x, check_y);
+
+        var polygon = [new Point(this.init_x, this.init_y),
+                    new Point(this.end_x, this.end_y),
+                    new Point(this.init_y, this.end_y),
+                    new Point(this.init_x, this.end_x)];
+
+        for (list_element in polygon) {
+            createAndDrawCircles(polygon[list_element].x, polygon[list_element].y);
+        }
+
+        if (check_x && check_y){
+            console.log('object selected');
+        }
     }
 }
 
@@ -24,45 +64,41 @@ function Circle(init_coordinates) {
     this.draw = function() {
         console.log('Drawing Circle');
         context.beginPath();
-        context.arc(this.init_x, this.init_y, 50, 0, 2 * Math.PI); //(center_x, center_y, start_angle, end_angle)
+        context.arc(this.init_x, this.init_y, 1, 0, 2 * Math.PI); //(center_x, center_y, start_angle, end_angle)
         context.closePath();
         context.strokeStyle = 'red';
         context.stroke();
     }
 }
 
+// Code Functions
 var drawAllObjects = function(objects_list) {
     for (object_item in objects_list) {
         objects_list[object_item].draw();
     }
 }
 
-// Init
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+var createAndDrawCircles = function(x, y) {
+    var new_circle = new Circle([x, y])
+    new_circle.draw();
+}
 
-const WIDTH = window.innerWidth;
-const HEIGHT = window.innerHeight;
-context.canvas.width  = WIDTH;
-context.canvas.height = HEIGHT;
-canvas.style.backgroundColor = 'aquamarine';
+canvas.addEventListener('click', function(event) {
+    var x = event.pageX, y = event.pageY;
 
-var bar_list = [];
-var circle_list = [new Circle([100,600])];
+    for (object_item in bar_list) {
+        bar_list[object_item].check_area([x, y]);
+    }
+
+}, false);
 
 // Code
+bar_list = [new Bar([100,50], [500,50]), new Bar([100,900], [900,100])]
+bar_list.push(new Bar([400,100], [400,400]));
 
-var test_bar1 = new Bar([10,10], [50,50]);
-var test_bar2 = new Bar([80,80], [130,130]);
-var test_bar3 = new Bar([60,35], [10,90]);
-
-bar_list = [test_bar1, test_bar2, test_bar3]
-var test_bar4 = new Bar([400,100], [100,250]);
-bar_list.push(test_bar4);
-
-console.log(circle_list);
+//console.log(circle_list);
 
 drawAllObjects(bar_list);
-drawAllObjects(circle_list);
+//drawAllObjects(circle_list);
 
 
