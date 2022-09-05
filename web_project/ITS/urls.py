66abe47d93c2bc763bfd8572cbbf1109f7c0f44c
jaime7981 <jmfernandez1@miuandes.cl>
2022-09-05 12:51:36 -0400

    assignment picture

--- web_project/ITS/urls.py
+++ web_project/ITS/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('usercontrol/', include('usercontrol.urls')),
     path('', RedirectView.as_view(url='dcl/', permanent=True)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
