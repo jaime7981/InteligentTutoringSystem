90df849a9ce6b166cf3b30aca03bc8cd016c76be
jaime7981 <jmfernandez1@miuandes.cl>
2022-09-05 11:46:50 -0400

    admin subscribe solution model

--- web_project/DCL/admin.py
+++ web_project/DCL/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Assignment
+from .models import Assignment, Solution
 
 # Register your models here.
 admin.site.register(Assignment)
+admin.site.register(Solution)
