b5d06b1df97100ed349e7c31148888897851c06d
jaime7981 <jmfernandez1@miuandes.cl>
2022-09-08 01:07:54 -0400

    upload filepath

--- web_project/DCL/models.py
+++ web_project/DCL/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from usercontrol.models import Teacher, Student
+import os
+
+def get_upload_path(instance, filename):
+    return os.path.join("assignment/%s" % instance.teacher.user.username, filename)
+
 # Create your models here.
 class Assignment(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -8,7 +13,7 @@ class Assignment(models.Model):
     level = models.IntegerField(blank=True, null=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     dcl_json = models.JSONField()
-    photo = models.ImageField(upload_to='assignment/', blank=True, null=True)
+    photo = models.ImageField(upload_to=get_upload_path, blank=True, null=True)
 
     def __str__(self):
         return str(self.dcl_json)
